a58f389f4021cf956a828f52f24bd5136fe22540
Eliel Moura <eliellpf@gmail.com>
2023-08-25 08:55:00 -0300

    sanitizando

--- fabricantes/inserir.php
+++ fabricantes/inserir.php
@@ -1,3 +1,14 @@
+<?php
+/* Verificando se o formulário/botão foi acionado*/
+if(isset($_POST['inserir'])){
+    // Capturando o valor digitado do nome e sanitizando
+    $nome = filter_input(INPUT_POST, "nome", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+// pode ser assim tbm
+   // $nome = filter_var($_POST["nome"], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -11,9 +22,10 @@
     <form action="" method="post">
         <p>
             <label for="nome">Nome:</label>
-            <input type="text" name="nome" id="nome">
+            <input required type="text" name="nome" id="nome">
         </p>
         <button type="submit" name="inserir">Inserir fabricante</button>
     </form>
+
 </body>
 </html>
