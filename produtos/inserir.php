7443009941ba5eb0bb4ec43b96648cafb2eb47e1
Eliel Moura <eliellpf@gmail.com>
2023-08-30 09:42:14 -0300

    inserindo produtos

--- produtos/inserir.php
+++ produtos/inserir.php
@@ -1,6 +1,28 @@
 <?php
 require_once"../src/funcoes-fabricantes.php";
+require_once"../src/funcoes-produtos.php";
 $listaDeFabricantes = lerFabricantes($conexao);
+if (isset($_POST['inserir'])) {
+    $nome = filter_input(INPUT_POST,"nome", FILTER_SANITIZE_SPECIAL_CHARS);
+    $preco = filter_input(
+        INPUT_POST, "preco",
+        FILTER_SANITIZE_NUMBER_FLOAT,
+        FILTER_FLAG_ALLOW_FRACTION
+    );
+
+    $quantidade = filter_input(INPUT_POST, "quantidade", FILTER_SANITIZE_NUMBER_INT);
+
+    //pegaremos o value, ou seja, o valor do id do fabricante
+    $fabricanteId = filter_input(
+INPUT_POST, "fabricante", FILTER_SANITIZE_NUMBER_INT
+    );
+
+    $descricao = filter_input(INPUT_POST,"descricao", FILTER_SANITIZE_SPECIAL_CHARS);
+
+    inserirProduto($conexao, $nome, $preco, $quantidade, $fabricanteId, $descricao);
+
+    header("location:visualizar.php");
+}
 ?> 
 
 <!DOCTYPE html>
@@ -20,11 +42,11 @@ $listaDeFabricantes = lerFabricantes($conexao);
     </p>
     <p>
         <label for="preco">Preço:</label>
-        <input type="number" min="1" max="100"  name="quantidade" id="quantidade" required>
+        <input type="number" min="10" max="10000"  name="preco" id="preco" required>
     </p>
     <p>
         <label for="quantidade">Quantidade:</label>
-        <input type="number" min="10" max=10000 step="0.01"  name="preco" id="preco" required>
+        <input type="number" min="1" max="100" step="0.01"  name="quantidade" id="quantidade" required>
     </p>
     <label for="fabricante">Fabricante:</label>
     <select name="fabricante" id="fabricante">
